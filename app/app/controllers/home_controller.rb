4d3d26b73dee11ae857120c7d324f3fa25c1c5e9
André Leoni <andreluizleoni@gmail.com>
2019-06-05 22:25:47 -0300

    Shows when service is unabailable

--- app/app/controllers/home_controller.rb
+++ app/app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @inbox = RestClient.get("#{ENV["MESSAGES_MS_URL"]}/v1/messages")
+    @messages = load_messages
+  end
+
+  private
+
+  def load_messages
+    RestClient.get("#{ENV["MESSAGES_MS_URL"]}/v1/messages")
+  rescue
+    "Service unavailable"
   end
 end
